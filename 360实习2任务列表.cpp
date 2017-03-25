bf1c6603e6490596e9851b526870847d56d1beec
xingyunbo <wangbo_zju@126.com>
2017-03-25 20:32:18 +0800

    360

--- 360实习2任务列表.cpp
+++ 360实习2任务列表.cpp
@@ -20,9 +20,11 @@ int main()
 		cin >> Q[i];
 
 		int left = 0, right = N - 1;
-		int mid = left + (right - left) / 2;
+		int mid;
 		bool QinT = false;
 		while (left <= right) {
+//cout << left << " " << right <<endl;
+			mid = left + (right - left) / 2;
 			if (Q[i] == T[mid]) { QinT = true; break; }
 			else if (Q[i] > T[mid]) left = mid + 1;
 			else right = mid - 1;
@@ -30,14 +32,18 @@ int main()
 		
 		if (QinT) {
 			for (int j = 0; j < tmp.size(); j++) {
-				if (Q[i] <= tmp[j]) { res.push_back(tmp[j]); break; }
-				if (j == tmp.size() - 1) res.push_back(Q[i]);
+//				if (Q[i] <= tmp[j]) { res.push_back(tmp[j]); break; }
+//				if (j == tmp.size() - 1) res.push_back(Q[i]);
+
+				if (Q[i] <= tmp[j]) { cout << tmp[j] << endl; break; }
+				if (j == tmp.size() - 1) cout << Q[i] << endl;
 			}
 		}
-		else res.push_back(Q[i]);
+//		else res.push_back(Q[i]);
+		else cout << Q[i] << endl;
 	}
 	
-	for (int i = 0; i < M; i++)
-		cout << res[i] << endl;
+//	for (int i = 0; i < M; i++)
+//		cout << res[i] << endl;
 	return 0;
 }
