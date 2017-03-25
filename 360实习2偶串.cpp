3ab5ee733e65c5b075ff43882d3b7151f93630b2
xingyunbo <wangbo_zju@126.com>
2017-03-25 21:05:23 +0800

    360

--- 360实习2偶串.cpp
+++ 360实习2偶串.cpp
@@ -9,21 +9,21 @@ int main()
 	int cnt = 0;
 	vector<int> hash(26, 0);
 	int n = t.size();
-	for (int i = 2; i <= n; i += 2) {
-		for (int j = 0; j + i <= n; j++) {
-			bool isEven = true;
-			for (int k = 0; k < i; k++) {
-				hash[t[j+k] - 'a']++;
-printf("hash[%c] = %d\n", t[j+k], hash[t[j+k] - 'a']);
-printf("i = %d, j = %d, k = %d, cnt = %d\n", i, j, k, cnt);
+	for (int j = 0; j < n; j++) {
+		for (int i = j; i < n; i++) {
+			hash[t[i] - 'a']++;
+			if ((i - j) % 2) {
+				bool isEven = true;
+				for (int k = 0; k < 26; k++) {
+					if (hash[k] % 2 != 0) isEven = false;
+				}
+				if (isEven) cnt++;
 			}
-			for (int x = 0; x < 26; x++) {
-				if (hash[x] % 2 != 0) isEven = false;
-				hash[x] = 0;
-			}
-			if (isEven) cnt++;
 		}
+
+		for (int k = 0; k < 26; k++) hash[k] = 0;
 	}
+
 	cout << cnt << endl;
 	return 0;
 }
