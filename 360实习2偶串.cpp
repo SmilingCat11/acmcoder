8cf96c8360accd81a3e49a12906eefa10b2a2818
xingyunbo <wangbo_zju@126.com>
2017-03-31 11:54:15 +0800

    360

--- 360实习2偶串.cpp
+++ 360实习2偶串.cpp
@@ -7,23 +7,14 @@ int main()
 	string t;
 	cin >> t;
 	int cnt = 0;
-	vector<int> hash(26, 0);
 	int n = t.size();
 	for (int j = 0; j < n; j++) {
+		long long res = 0;
 		for (int i = j; i < n; i++) {
-			hash[t[i] - 'a']++;
-			if ((i - j) % 2) {
-				bool isEven = true;
-				for (int k = 0; k < 26; k++) {
-					if (hash[k] % 2 != 0) isEven = false;
-				}
-				if (isEven) cnt++;
-			}
+			res ^= 1 << (t[i] - 'a');
+			if (!res) cnt++;
 		}
-
-		for (int k = 0; k < 26; k++) hash[k] = 0;
 	}
-
 	cout << cnt << endl;
 	return 0;
 }
